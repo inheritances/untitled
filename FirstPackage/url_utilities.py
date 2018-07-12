da94af3cbc793febcb4e6abcf97093f0f6c6351b
adam.wahidin <givedont@gmail.com>
2018-07-12 13:49:30 +0700

    finish add url_utilities.py

--- FirstPackage/url_utilities.py
+++ FirstPackage/url_utilities.py
@@ -1,4 +1,8 @@
+import re
+import string
 from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
 
 
 def load_file_from_url(file_path: str):
@@ -7,16 +11,55 @@ def load_file_from_url(file_path: str):
             content = f.readline()
             return content
     except FileNotFoundError:
-        print("the file {0} couldn\'t be found".format(file_path));
-
+        print("the file {0} couldn\'t be found".format(file_path))
         exit(2)
 
 
 def load_page(url: str):
-    respone = urlopen(url)
-    html = respone.read().decode('utf-8')
+    response = urlopen(url)
+    html = response.read().decode('utf-8')
     return
 
+
 def scrape_page(page_contents: str):
-    # TODO: analyze the text
-    pass
+    chicken_noodle = BeautifulSoup(page_contents, "html5lib")
+
+    for script in chicken_noodle(["script", "style"]):
+        script.extract()
+
+    text = chicken_noodle.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    plain_text = ''.join(filter(lambda x: x in string.printable, text))
+
+    clean_words = []
+
+    words = plain_text.split(" ")
+    for word in words:
+        clean = True
+
+        # no punctuation
+        for punctutation_marks in string.punctuation:
+            if punctutation_marks in word:
+                clean = False
+
+                # no numbers
+            if any(char.isdigit() for char in word):
+                clean = False
+
+                # at least two characters but no more than 10
+            if len(word) < 2 or len(word) > 10:
+                clean = False
+
+            if not re.match(r'^\w+$', word):
+                clean = False
+
+            if clean:
+                try:
+                    clean_words.append(word.lower())
+                except UnicodeEncodeError:
+                    print(".")
+
+        return clean_words
