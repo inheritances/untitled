e00464e20b4fe5036d28eab6a100cb097c9e711c
adam.wahidin <givedont@gmail.com>
2018-07-12 12:52:37 +0700

    add utilities

--- FirstPackage/url_utilities.py
+++ FirstPackage/url_utilities.py
@@ -1,12 +1,21 @@
+from urllib.request import urlopen
+
+
 def load_file_from_url(file_path: str):
-    # TODO: add the code to read the textfile with the url it
-    pass
+    try:
+        with open(file_path) as f:
+            content = f.readline()
+            return content
+    except FileNotFoundError:
+        print("the file {0} couldn\'t be found".format(file_path));
+
+        exit(2)
 
 
 def load_page(url: str):
-    # TODO: add the for load first url Page
-    pass
-
+    respone = urlopen(url)
+    html = respone.read().decode('utf-8')
+    return
 
 def scrape_page(page_contents: str):
     # TODO: analyze the text
