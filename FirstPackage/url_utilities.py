86b09d63248e71ecb259fa19132f8179c207e2e4
adam.wahidin <givedont@gmail.com>
2018-07-12 12:33:06 +0700

    fixing formating issue

--- FirstPackage/url_utilities.py
+++ FirstPackage/url_utilities.py
@@ -1,11 +1,13 @@
-def load_file_from_URL(file_path:str):
-    #TODO: add the code to read the textfile with the url it
+def load_file_from_url(file_path: str):
+    # TODO: add the code to read the textfile with the url it
     pass
 
-def load_page(url:str):
-    #TODO: add the for load first url Page
+
+def load_page(url: str):
+    # TODO: add the for load first url Page
     pass
 
-def scrape_page(page_contents:str):
-    #TODO: analyze the text
+
+def scrape_page(page_contents: str):
+    # TODO: analyze the text
     pass
